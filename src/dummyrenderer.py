8c05ffc06327af2eba77512a421a116c04642c7f
David Shere <david.e.shere@gmail.com>
2013-11-14 08:52:52 -0800

    Added comments on dummyrenderer.py

--- src/dummyrenderer.py
+++ src/dummyrenderer.py
@@ -1,10 +1,30 @@
+"""
+.. module:: dummyrenderer
+   :synopsis: This module contains an example Renderer for MPIScope.
+
+.. moduleauthor:: David E. Shere <david.e.shere@gmail.com>
+
+"""
+
 class DummyRenderer:
+    """ This class is a mimimal implementation of a Renderer
+        for MPIScope.
+    """
 
     def init(self):
+        """Do any initialization required
+        """
         print("init")
 
     def draw(self, jobData):
-        print(jobData.keys())
+        """Do any drawing you want displayed.
+           This method is only called when the
+           UpdateThread actually has data (IE: is not None)
+        """
+        print("draw")
 
     def flip(self):
+        """Do anything that needs to happen after drawing.
+           This method is run every tick even if there is no data.
+        """
         return
