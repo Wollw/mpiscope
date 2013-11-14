1dcffcb3c8ea12294fafac189c141126a9911d33
David Shere <david.e.shere@gmail.com>
2013-11-14 08:48:30 -0800

    commenting added on mpiscope.py

--- src/mpiscope.py
+++ src/mpiscope.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python2
+"""
+.. module:: mpiscope
+   :synopsis: A module that provides classes for facilitating
+              parallel graphics programs using MPI.
+
+.. moduleauthor:: David E. Shere <david.e.shere@gmail.com>
+
+"""
 import copy
 import json
 import threading
@@ -8,8 +15,24 @@ import urllib2
 from mpi4py import MPI
 
 class MPIScope:
+    """Provides a scope for running a parallel graphics program.
+    
+    Although this class is intended for use in graphics programs
+    it could be used for other purposes.  The renderer.draw method
+    simply runs every tick if there is available data and
+    renderer.flip runs every tick.
+
+    """
 
     def __init__(self, renderer, urlList):
+        """Initialize MPI and the UpdateThread and store the renderer
+
+        Args:
+            renderer (Renderer): An object used to display fetched data.
+            urlList ({str}): A dictionary of strings with names as keys.
+
+        """
+
         self.comm = MPI.COMM_WORLD
         self.rank = self.comm.Get_rank()
         self.renderer = renderer
@@ -18,6 +41,8 @@ class MPIScope:
         self.updateThread = UpdateThread(self.lock, urlList)
         
     def run(self):
+        """ Start up and run the renderer and updateThread.
+        """
         self.updateThread.start()
         self.renderer.init()
         while 1:
@@ -29,10 +54,25 @@ class MPIScope:
             self.comm.barrier()
             self.renderer.flip()
 
-
 class UpdateThread(threading.Thread):
+    """ This class is used to create a separate thread
+        for fetching and presenting data that will be rendered.
+
+    """
 
     def __init__(self, lock, urlList, delay=60):
+        """ Initialize the thread and prepare it for reading and
+            storing the data from the urlList urls.
+
+            Args:
+                lock (Lock): Lock for maintaining thread safety.
+                urlList ({str}): Dictionary of URLs to fetch data from.
+            
+            Kwargs:
+                delay (int): The number of seconds to wait between fetches.
+
+        """
+
         threading.Thread.__init__(self)
         self.status = -1
         self.lock = lock
@@ -42,6 +82,8 @@ class UpdateThread(threading.Thread):
         self._updated = True
 
     def run(self):
+        """ Start fetching data from the urls.
+        """
         while 1:
             requests = { name: urllib2.urlopen(url)
                        for name, url
@@ -64,6 +106,9 @@ class UpdateThread(threading.Thread):
 
             time.sleep(self.delay)
 
+    """ Returns true if the data was updated;
+        this resets to false when checked.
+    """
     def updated(self):
         with self.lock:
             if self._updated:
