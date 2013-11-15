25d386a8f1eeec2d66b8a504834155d1c4f74857
David E. Shere <david.e.shere@gmail.com>
2013-11-15 15:36:10 -0800

    Reorganized package.

--- README.md
+++
@@ -1,2 +0,0 @@
-sdsc-data-visualization
-=======================
---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='MPIScope',
+    version='0.1.0',
+    author='David E. Shere',
+    author_email='david.e.shere@gmail.com',
+    packages=['mpiscope'],
+    scripts=['bin/example.py'],
+    #url=[''],
+    license="LICENSE.txt",
+    description='Provides a context for MPI based graphics programs.',
+    long_desciption=open('README.txt').read(),
+    install_requires=[
+        "mpi4py"
+    ],
+)
+
