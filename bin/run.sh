b4dc66cd61eb6d035b07be8e9d96681346bde16d
David E. Shere <david.e.shere@gmail.com>
2013-11-18 18:06:32 -0800

    Added OS check for Mac OS X to use openmpirun instead of mpirun

--- bin/run.sh
+++ bin/run.sh
@@ -1,1 +1,9 @@
-mpirun --hostfile etc/hostfile.local -np 3 python2 bin/example.py
+#!/usr/bin/env bash
+
+if [[ $OSTYPE == darwin* ]]; then
+    MPIRUN=openmpirun;
+else
+    MPIRUN=mpirun;
+fi
+
+$MPIRUN --hostfile etc/hostfile.local -np 3 python2 bin/example.py
