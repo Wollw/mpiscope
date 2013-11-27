041774370840698e7e70c9f3bbc52724d2800d83
David E. Shere <david.e.shere@gmail.com>
2013-11-27 13:55:13 -0800

    Conditional python version in run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ $OSTYPE == darwin* ]]; then
+    PYTHON=python
     MPIRUN=openmpirun;
 else
+    PYTHON=python2
     MPIRUN=mpirun;
 fi
 
-$MPIRUN --hostfile etc/hostfile.local -np 3 python2 bin/example.py
+$MPIRUN --hostfile etc/hostfile.local -np 3 $PYTHON bin/example.py
